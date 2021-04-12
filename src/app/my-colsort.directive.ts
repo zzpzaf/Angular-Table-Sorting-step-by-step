1265b65c73803b9f66368dc10e46b58806047bca
Panos Zafeiropoulos <zzpzaf@gmail.com>
2021-04-12 19:06:45 +0300

    Applying an Attribute Directive and using css :after for colored text indicators in column headers for sorting direction

--- src/app/my-colsort.directive.ts
+++ src/app/my-colsort.directive.ts
@@ -1,9 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appMyColsort]'
+  selector: '[appMyColsort]',
+  host: {'[class.colorsortasc]': 'direct==="asc"',
+         '[class.colorsortdesc]': 'direct==="desc"'}
 })
 export class MyColsortDirective {
+
+
+  @Input() direct: string = '';
+
 
   constructor() { }
 
--- src/app/my-table/my-table.component.ts
+++ src/app/my-table/my-table.component.ts
@@ -1,6 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
 
 
 interface Emp {  
@@ -40,16 +39,18 @@ const EMPLOYEES: Emp[] = [
 
 const headerFields: string[] = ['First Name','Last Name', 'Age', 'Date Joined'];
 const fieldsNames: string[] = ['firstName','lastName', 'age', 'joinedDate'];
-const arrDirections = ['asc', 'desc', ''] ;
+const arrDirectionOptios = ['asc', 'desc', ''] ;
+
 
 // IT Works: Union type with predefined literal values from array const //Angular version 3.4 and onwards
-type SortDirection = typeof arrDirections[0] | typeof arrDirections[1] | typeof arrDirections[2]; 
+type SortDirection = typeof arrDirectionOptios[0] | typeof arrDirectionOptios[1] | typeof arrDirectionOptios[2]; 
 
 //const rotatedirection: {[key in SortDirection]: SortDirection} = { asc: 'desc', desc: '', '': 'asc' };
-const rotate: {[key in SortDirection]: SortDirection} = { asc: arrDirections[1], desc: '', '': arrDirections[0] };
+const rotate: {[key in SortDirection]: SortDirection} = { asc: arrDirectionOptios[1], desc: '', '': arrDirectionOptios[0] };
 
-
-
+// This is a more generan Comparison function for ascending order sorting
+// We cam always reverse the result for descending order.
+const compareAsc = (v1: any, v2: any) => v1 > v2 ? 1 : v1 < v2 ? -1 : 0;
 
 
 @Component({
@@ -63,23 +64,51 @@ export class MyTableComponent implements OnInit {
   public empls: Emp[] = [];
   public hfs: string[] = headerFields;
   public fns: string[] = fieldsNames;
-  public hfsd: string[] = ['','','',''];
+  //public hfsd: string[] = ['','','',''];
+
+  //public direction!: SortDirection; 
+  public colDirections: SortDirection[] = [] ;  
+
+  //@ViewChildren(MyColsortDirective) myList!: QueryList<MyColsortDirective>;
 
 
-  private direction!: SortDirection; // Initial is 'asc'--> ascending order,   ''' --> not ordered
-  private namesArr: string[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
 
     this.empls = EMPLOYEES;  
-    this.direction = arrDirections[0]
+    this.colDirections = ['','',''];    // Initial is '' --> not ordered,
+    //this.direction = arrDirectionOptios[2]  // Initial is '' --> not ordered, asc'--> ascending order, etc. 
 
   }
 
   onSort(column: string) {
-    this.setSortIndication(column);
-    this.empls = this.sort(EMPLOYEES, column, this.direction);
+
+    //this.colDirections = ['','',''];
+    let s : number = this.fns.indexOf(column);
+    this.colDirections[s] = rotate[this.colDirections[s]];
+
+    let i : number;
+    for(i = 0; i<this.fns.length; i++) {
+      if (i !== s) {
+        this.colDirections[i] = '';
+      };
+   }
+    
+
+    // resetting other headers
+    // this.myList.forEach(listProp => {
+    //   if (listProp.colname !== column) {
+    //     listProp.direct = '';
+    //   }
+    //   console.log(column, listProp.colname, listProp.direct);
+    // });
+
+
+    this.empls = this.sort(EMPLOYEES, column, this.colDirections[s]);
+
+
   }
 
 
@@ -89,27 +118,63 @@ export class MyTableComponent implements OnInit {
     if (direction === '') {
       sortedEmployees = inEmpls; 
     } else {
-       if (direction === arrDirections[1]) {
+      sortedEmployees = [...inEmpls].sort((a,b) => {
+        const res = compareAsc(a[field as keyof Emp], b[field as keyof Emp]);
+        return (direction === arrDirectionOptios[1])? -res : res;   //If it is  'desc'
+       });   }
+       //this.direction = rotate[this.direction]; 
+   
+    return sortedEmployees; 
+  }	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  sort1(inEmpls: Emp[], field: string, direction: SortDirection): Emp[] {
+  
+    let sortedEmployees: Emp[] = [];
+    if (direction === '') {
+      sortedEmployees = inEmpls; 
+    } else {
+       if (direction === arrDirectionOptios[1]) {
           sortedEmployees = [...inEmpls].sort((a,b) => (a[field as keyof Emp] < b[field as keyof Emp] ? 1 : -1)); 	
        } else {
           sortedEmployees = [...inEmpls].sort((a,b) => (a[field as keyof Emp] < b[field as keyof Emp] ? -1 : 1));
        }
     }
-    this.direction = rotate[this.direction];
     return sortedEmployees; 
   }	
 
 
-  setSortIndication(col: string):void{
 
-    for (var i in this.hfs) {
-      if (col === this.fns[i]) {
-        this.hfsd[i] = (this.direction === '') ? '' : '[' + this.direction + ']';
-      }else {
-        this.hfsd[i] = '';
-      }
-    }
 
-  }
+
+  // setSortIndication(col: string):void{
+
+  //   for (var i in this.hfs) {
+  //     if (col === this.fns[i]) {
+  //       this.hfsd[i] = (this.direction === '') ? '' : '[' + this.direction + ']';
+  //     }else {
+  //       this.hfsd[i] = '';
+  //     }
+  //   }
+
+  // }
 
 }
